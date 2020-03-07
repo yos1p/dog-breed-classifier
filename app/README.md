4562da69fbf7f18e74c875211cbdfbe13a89dc9e
iamyaoxi <yosi.pramajaya@gmail.com>
2020-03-07 23:15:08 +0700

    Initial web app

---
+++ app/README.md
@@ -0,0 +1,8 @@
+## Doggaire Web App
+
+### Overview
+
+This is a simple Node.js application that will receive image from Client, make a prediction, and return the prediction. The model that we use is the same model in this project, the model is converted to be used by Tensorflow.js
+
+
+TensorFlow, the TensorFlow logo and any related marks are trademarks of Google Inc.
---
+++ app/index.js
@@ -0,0 +1,59 @@
+const tf = require('@tensorflow/tfjs')
+const tfn = require('@tensorflow/tfjs-node')
+const fileUpload = require('express-fileupload')
+var fs = require("fs")
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.use(fileUpload());
+
+const handler = tfn.io.fileSystem("./tfjs_model/model.json")
+app.post('/predict', (req, res) => {
+    if (!req.files) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let image = Uint8Array.from(req.files.doggo.data)
+    image = tfn.node.decodeImage(image, 3)
+    image = tf.image.resizeBilinear(image, [224, 224]).toFloat()
+    image = image.div(255)
+    image = image.expandDims(0)
+
+    tf.loadLayersModel(handler, { strict: false }).then((model) => {
+        console.log('Model loaded')
+        prediction = model.predict(image)
+        let { values, indices } = tf.topk(prediction, 3, true)
+        values = values.arraySync()[0]
+        indices = indices.arraySync()[0]
+
+        let text = fs.readFileSync('./breed_names.txt', 'utf-8')
+        let breeds = text.split("\n")
+
+        let predictions = []
+
+        for (i = 0; i < 3; i++) {
+            if (values[i] > 0.01) {
+                predictions.push({
+                    breed: breeds[indices[i]],
+                    pct: (values[i] * 100).toFixed(2)
+                })
+            }
+        }
+
+        res.json(predictions)
+    }).catch((err) => {
+        console.error(err)
+        res.send("Prediction error!")
+    })
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+app.listen(port, () => console.log(`Doggaire listening on port ${port}!`))
